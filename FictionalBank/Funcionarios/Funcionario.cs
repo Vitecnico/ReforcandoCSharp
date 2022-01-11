ff8d08da1b4bba61091db3de8cfe19a9e78e5b90
vitor <vitor@DESKTOP-GAJ2C41>
2022-01-11 10:48:05 -0300

    Adicionar arquivos de projeto.

---
+++ FictionalBank/Funcionarios/Funcionario.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FictionalBank.Funcionarios
+{
+    public class Funcionario
+    {
+        public string Name { get; set; }
+        public int Idade { get; set; }
+        public double Salario { get; set; }
+
+        public Funcionario(string name,int idade, double salario)
+        { 
+            Name = name;
+            Idade = idade;
+            Salario = salario;
+        
+        }
+    }
+}
---
+++ FictionalBank/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FictionalBank.Funcionarios;
+
+namespace FictionalBank
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Funcionario bruno = new Funcionario("Bruno", 34, 2500);
+            Console.WriteLine(123 - 321);
+            Console.WriteLine(bruno.Name);
+            Console.WriteLine(bruno.Idade);
+            Console.ReadLine();
+            
+        }
+    }
+}
